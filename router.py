8197777f28123851d42eae8a423a796b29d7208a
Steven Culwell <beastwick18@gmail.com>
2023-04-16 00:26:10 -0500

    Added some comments, fixed update print statement, removed unnecessary code

--- router.py
+++ router.py
@@ -125,10 +125,9 @@ def bellman_ford(table, source):
     
     return distance
 
-# Given some data sent from an `sender`, update the table with new values present in `data`
-def update_table(sender, data):
-    new_table = data
-    
+# Given some data sent from an `sender`, update the table with new values present in `new_table`
+def update_table(sender, new_table):
+    # Go through each cost and replace it with the updated table's cost if it is lower
     updated = False
     for node, edges in new_table.items():
         for edge, cost in edges.items():
@@ -136,17 +135,17 @@ def update_table(sender, data):
                 print(f'Updated: Source={sender}, Current={cost}, Previous={table[node][edge]}')
                 updated = True
                 table[node][edge] = cost
-    
+
+    # Perform the bellman ford algorithm to replace costs with the cost to reach by traversing one node ahead, but only if it is a lower cost
     distance = bellman_ford(table, get_index(ID))
     for i, cost in enumerate(distance):
         v = NODES[i]
         if cost < table[ID][v]:
-            print(f'Updated: Source={sender}, Current={table[ID][v]}, Previous={cost}')
+            print(f'Updated: Source={sender}, Current={cost}, Previous={table[ID][v]}')
             table[ID][v] = cost
             updated = True
     
-    # if updated:
-        # print_table(table)
+    # Return whether or not any changes were made, so that we can decide whether or not to update our neighbors
     return updated
 
 # Send an update each node that shares an edge with this node
@@ -164,43 +163,64 @@ def convergence(table):
                 return False
     return True
 
+# Perform a router simulation.
+# wait_for_broadcast indicates if we should wait for at least one broadcast to be recieved before
+# considering whether or not convergence has been achieved. This is used for when we are expecting
+# a link break to occur while the router may have aleardy reached convergence.
+# initial_broadcast contains a broadcast that we are meant to send out on the first update. This is
+# used for when we are meant to initially broadcast a link break.
 def router_simulation(sock, wait_for_broadcast = False, initial_broadcast = None):
     print('Press `Ctrl + C` to exit\nListening...')
     
-    broadcast_index = 0
+    # Initialize broadcast_msgs to contain the initial_broadcast, only if it is present
     if initial_broadcast is not None:
         print(f'Initially broadcast {initial_broadcast}')
+        # The broadcast must be populated with all the edges that this node contains, so that we
+        # can check them off for acknowledgement
         broadcast_msgs = {initial_broadcast: [edge for edge in edges]}
     else:
         broadcast_msgs = {}
+    
+    # Keep track of broadcasts we have already sent and recieved acknowledgement for so that we do
+    # not send them out again
     old_broadcasts = []
+    
+    # Keep track of the total number of updates to the table
     update_count = 0
-    # Continue to run while we have not converged
+    
+    # Continue to run while we have not converged, or while we are waiting for a broadcast
     while wait_for_broadcast or not convergence(table):
         try:
             # Try to receive a message
             msg_type, id, data = recieve_message(sock)
             
-            # First check if we are meant to wait for acknowledgement of previous broadcasts
+            # First check if we have recieved an acknowledgement from one of our peers
             if msg_type == 'ack':
+                # Check that we are still waiting for an acknowledgement from this peer for
+                # this specific broadcast
                 if data in broadcast_msgs and id in broadcast_msgs[data]:
                     # Remove them from the pending acknowledgements
                     broadcast_msgs[data].remove(id)
                     print(f'Ack from {id}')
                     
-                    # If we require no more acknowledgements, remove this broadcast from the list
+                    # If we require no more acknowledgements, move this broadcast to old_broadcasts
                     if not broadcast_msgs[data]:
                         print(f'{data} successfully fully broadcasted')
                         del broadcast_msgs[data]
                         old_broadcasts.append(data)
-                        broadcast_index = 0
+            # Otherwise, check if we have recieved a notice that a link was broken.
             elif msg_type == 'link_broken':
+                # First check if we are aware of this and still waiting for acknowledgement
                 if data in broadcast_msgs:
+                    # We are aware already, so acknowledge
                     send_message(sock, id, 'ack', data)
+                # Otherwise, check that we haven't already seed this at all
                 elif data not in old_broadcasts:
                     # Recieved new broadcast
                     wait_for_broadcast = False
                     print(f'New broadcast from {id}')
+                    
+                    # Add this broadcast to the list of broadcasts
                     broadcast_msgs[data] = [edge for edge in edges]
                     broadcast_msgs[data].remove(id)
                     send_message(sock, id, 'ack', data)
@@ -208,7 +228,6 @@ def router_simulation(sock, wait_for_broadcast = False, initial_broadcast = None
                         print(f'{data} successfully fully broadcasted')
                         del broadcast_msgs[data]
                         old_broadcasts.append(data)
-                        broadcast_index = 0
                         
                     
                     print(f'A link was broken from {data[0]} to {data[1]}, reset table')
@@ -236,20 +255,20 @@ def router_simulation(sock, wait_for_broadcast = False, initial_broadcast = None
                 raise TimeoutError
             sleep(TIMEOUT)
         except TimeoutError:
+            # If we still have messages to broadcast, broadcast one at the head of the list
+            # to each neighbor that we are missing an acknowledgement from
             if broadcast_msgs:
-                msg = list(broadcast_msgs.items())[broadcast_index][0]
+                msg = list(broadcast_msgs.items())[0][0]
                 for neighbor in broadcast_msgs[msg]:
-                    print(f'Rebroadcast {broadcast_index} to {neighbor}: {("link_broken", ID, msg)}')
+                    print(f'Rebroadcast {msg} to {neighbor}: {("link_broken", ID, msg)}')
                     send_message(sock, neighbor, 'link_broken', msg)
-                
-                broadcast_index = (broadcast_index + 1) % len(broadcast_msgs)
             else:
                 # Periodically update our neigbors
                 update_neighbors(sock)
     return update_count
 
 def test1(sock, update_count):
-    # Broadcast only for router A for now...
+    # Broadcast from router A
     if ID == 'A':
         sock.settimeout(TIMEOUT)
         print('\n-------------------------\nTest 1:')
@@ -318,16 +337,16 @@ def test2(sock):
     print_table(table)
     
     sock.settimeout(TIMEOUT)
-    if ID == 'B':
-        load_config(ID)
-        del edges['D']
-        table['B']['D'] = INFINITY
-        router_simulation(sock, False, ('B', 'D'))
-    elif ID == 'D':
+    if ID == 'A':
         load_config(ID)
         del edges['B']
-        table['D']['B'] = INFINITY
-        router_simulation(sock, False, ('D', 'B'))
+        table['A']['B'] = INFINITY
+        router_simulation(sock, False, ('A', 'B'))
+    elif ID == 'B':
+        load_config(ID)
+        del edges['A']
+        table['B']['A'] = INFINITY
+        router_simulation(sock, False, ('B', 'A'))
     else:
         router_simulation(sock, True)
     
